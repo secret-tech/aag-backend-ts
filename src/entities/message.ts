1b95ded0407cd4da6ab36765c543944af60b516d
Andrey Degtyaruk <hlogeon1@gmail.com>
2018-12-06 05:59:39 +0300

    Message format

--- src/entities/message.ts
+++ src/entities/message.ts
@@ -29,6 +29,8 @@ export class Message {
   constructor(sender: User, receiver: User, conversation: string, message: string) {
     this.timestamp = Date.now();
     this.createdAt = new Date();
+    console.log('Sender: ', sender);
+    console.log('conversation: ', conversation);
     this.user = new MessageUser(sender.id.toString(), sender.firstName, sender.picture);
     this.receiver = new MessageUser(receiver.id.toString(), receiver.firstName, receiver.picture);
     this.conversation = conversation;
