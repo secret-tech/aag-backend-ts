67df389a7a8af2c530a993476bd8a3a123c676fc
Andrey Degtyaruk <hlogeon1@gmail.com>
2018-12-06 06:15:01 +0300

    Message format

--- src/entities/message.ts
+++ src/entities/message.ts
@@ -1,6 +1,7 @@
 import { Column, Entity, ObjectIdColumn, ObjectID } from 'typeorm';
 import { User } from './user';
 import { MessageUser } from './message.user';
+import 'reflect-metadata';
 
 @Entity()
 export class Message {
@@ -17,11 +18,11 @@ export class Message {
   @Column()
   text: string;
 
-  @Column()
-  user: MessageUser;
+  @Column(type => MessageUser)
+  user?: MessageUser;
 
-  @Column()
-  receiver: MessageUser;
+  @Column(type => MessageUser)
+  receiver?: MessageUser;
 
   @Column(type => Date)
   createdAt: Date;
@@ -29,8 +30,8 @@ export class Message {
   constructor(sender: User, receiver: User, conversation: string, message: string) {
     this.timestamp = Date.now();
     this.createdAt = new Date();
-    this.user = new MessageUser(sender.id.toString(), sender.firstName, sender.picture);
-    this.receiver = new MessageUser(receiver.id.toString(), receiver.firstName, receiver.picture);
+    // this.user = new MessageUser(sender.id.toString(), sender.firstName, sender.picture);
+    // this.receiver = new MessageUser(receiver.id.toString(), receiver.firstName, receiver.picture);
     this.conversation = conversation;
     this.text = message;
   }
